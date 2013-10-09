e100be41e125ef6e4d200a459335efd0a44c9b95
mezzokoko <cptkeyes@gmx.de>
2013-10-09 09:54:31 +0200

    first upload of stable version

---
+++ gamemode/cl_hitlist.lua
@@ -0,0 +1,18 @@
+local targetname
+
+function ReceiveTarget(um)
+    targetname = um:ReadString()
+end
+usermessage.Hook( "hitman_newtarget", ReceiveTarget )
+
+function NoTarget(um)
+    targetname = nil
+end
+usermessage.Hook( "hitman_notarget", NoTarget )
+
+function DisplayTarget()
+    if targetname ~= nil then
+       			draw.SimpleText("Kill " .. targetname, "HealthAmmo", 40, ScrH() - 170, Color(255, 0, 0, 255))
+    end
+end
+hook.Add("HUDPaint", "DisplayTarget", DisplayTarget);
---
+++ readme-mezzo.txt
@@ -0,0 +1,5 @@
+INSTALLATION:
+Copy these files into your TTT-Gamemodefolder and overwrite the original ones (You probably should create a backup).
+
+DESCRIPTION:
+Same rules as TTT apply. But this time the traitors are each given a specific target which they have to eliminate.
