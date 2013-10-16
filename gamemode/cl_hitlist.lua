617cf7455c9649272ba9640ae18f6699f917d4df
mezzokoko <cptkeyes@gmx.de>
2013-10-16 15:28:26 +0200

    Optimized parts of the code; Prepared implementation for scoreboard-status

--- gamemode/cl_hitlist.lua
+++ gamemode/cl_hitlist.lua
@@ -13,11 +13,13 @@ local fp_link = "http://facepunch.com/showthread.php?t=1314066"
 local x = 270
 local y = ScrH() - 130
 
-local w = 200
+local w = 250
 local h = 120
 
 local function ReceiveTarget(um)
-    targetname = um:ReadString()
+    local ply = um:ReadEntity()
+    targetname = ply:Nick()
+    --Insert Scoreboard status stuff here!
 end
 usermessage.Hook( "hitman_newtarget", ReceiveTarget )
 
@@ -27,21 +29,21 @@ end
 usermessage.Hook( "hitman_notarget", NoTarget )
 
 local function DisplayHitlistHUD()
-    if targetname ~= nil and alive == 1 and traitor == 1 then
-		--basic box
+    if targetname ~= nil and alive and traitor then
+        --basic box
         draw.RoundedBox(8, x, y, w, h, Color(0, 0, 10, 200))
-		draw.RoundedBox(8, x, y, w, 30, Color(200, 25, 25, 200))
-		
-		--Didn't mind using BadKings ShadowedText. For some reason stuff doesn't properly import. Got to clean up the bloody code at some point anyway.
-		
-		--Target announcer
-		draw.SimpleText(targetname, "TraitorState", x + 12, y+2, Color(0, 0, 0, 255))
+        draw.RoundedBox(8, x, y, w, 30, Color(200, 25, 25, 200))
+        
+        --Didn't mind using BadKings ShadowedText. For some reason stuff doesn't properly import. Got to clean up the bloody code at some point anyway.
+        
+        --Target announcer
+        draw.SimpleText(targetname, "TraitorState", x + 12, y+2, Color(0, 0, 0, 255))
         draw.SimpleText(targetname, "TraitorState", x + 10, y, Color(255, 255, 255, 255))
-		--Stats
+        --Stats
         draw.SimpleText("Killed Targets: " .. targetkills, "HealthAmmo", x + 12, y +42, Color(0, 0, 0, 255))
-		draw.SimpleText("Killed Targets: " .. targetkills, "HealthAmmo", x + 10, y +40, Color(255, 255, 255, 255))
-		
-		draw.SimpleText("Killed Civilians: " .. civkills, "HealthAmmo", x + 12, y + 62, Color(0, 0, 0, 255))
+        draw.SimpleText("Killed Targets: " .. targetkills, "HealthAmmo", x + 10, y +40, Color(255, 255, 255, 255))
+        
+        draw.SimpleText("Killed Civilians: " .. civkills, "HealthAmmo", x + 12, y + 62, Color(0, 0, 0, 255))
         draw.SimpleText("Killed Civilians: " .. civkills, "HealthAmmo", x + 10, y + 60, Color(255, 255, 255, 255))
     end
 end
@@ -58,32 +60,19 @@ end
 usermessage.Hook( "hitman_killed_civs", SetCivKills )
 --Fetch condition, so the Display can work properly
 local function SetAlive(um)
-    alive = 1
-    print("You are now set as Alive")
+    alive = um:ReadBool()
 end
 usermessage.Hook( "hitman_alive", SetAlive )
 
-local function SetDead(um)
-    alive = 0
-end
-usermessage.Hook( "hitman_dead", SetDead )
-
 local function SetTraitor(um)
-    traitor = 1
-    print("You are now set as Alive")
+    traitor = um:ReadBool()
+    if traitor then YouAreTraitor() end
 end
-usermessage.Hook( "hitman_traitor", SetTraitor )
+usermessage.Hook( "hitman_hitman", SetTraitor )
 
-local function SetInnocent(um)
-    traitor = 0
-end
-usermessage.Hook( "hitman_innocent", SetInnocent )
-
-local function YouAreTraitor(um)
+function YouAreTraitor()
     chat.AddText(Color(255, 0, 0), "You are a hitman, hired by a mysterious employer who wants a range of people dead. Avoid killing anyone other than the target or your employer will be ... unsatisfied.")
 end
-usermessage.Hook( "hitman_you_are_t", YouAreTraitor )
-
 
 local function Disappointed(um)
     chat.AddText(Color(255, 0, 0), "Your employer is very disappointed of your work and decided to activate the killswitch")
@@ -93,5 +82,5 @@ usermessage.Hook( "hitman_disappointed", Disappointed )
 --Version stuff:
 local function PrintVersion()
     print("TTT Hitman Mod Version " .. hitman_version)
-	print("Latest Version available here: " .. fp_link)
+    print("Latest Version available here: " .. fp_link)
 end
